904726e87836d9d06b5583d205867fc391ac252e
Nick <nelom3@gmail.com>
2022-06-12 17:09:00 -0400

    commit tree 80bc568a66d0556e2546367b28aa4a6fb8a1b4bd

--- main.js
+++ main.js
@@ -4,8 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const hearts = document.querySelectorAll('.like-glyph')
 
+for (let i = 0; i < hearts.length; i++) {
+  hearts[i].addEventListener("click", function(e) {
+    const target = e.target;
+    mimicServerCall(target)
+    .then(responder(target))
+    .catch(error => errorFunc(error))
+  })
+}
 
+function responder(target) {
+  if (target.innerHTML === EMPTY_HEART) {
+    target.classList.add("activated-heart")
+    target.innerHTML = FULL_HEART;
+  } else {
+    target.classList.remove("activated-heart")
+    target.innerHTML = EMPTY_HEART;
+  }
+  
+}
+
+function errorFunc(error) {
+  const modal = document.getElementById("modal");
+  modal.classList.remove("hidden");
+  modal.innerHTML = error
+  setTimeout(() => {
+    modal.classList.add("hidden")
+  }, '3000')
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
